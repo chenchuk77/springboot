db8119c2ad2902e4b75a8c07c2a46b876b91abdc
chenchuk <chenchuk@gmail.com>
2018-11-21 23:56:59 +0200

    change versions, working spring boot simple app

--- src/main/java/io/net/kukinet/springbootstarter/CourseApiApp.java
+++ src/main/java/io/net/kukinet/springbootstarter/CourseApiApp.java
@@ -1,10 +1,13 @@
 package io.net.kukinet.springbootstarter;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class CourseApiApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// pass this class with the args
+		SpringApplication.run(CourseApiApp.class, args);
 	}
-
 }
