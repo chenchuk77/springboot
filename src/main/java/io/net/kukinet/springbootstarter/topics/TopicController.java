947bfe0d3cde24e3c55f1c02be0fc9db2383abcd
chenchuk <chenchuk@gmail.com>
2018-11-22 15:06:40 +0200

    adding singleton service, dependency innjection

--- src/main/java/io/net/kukinet/springbootstarter/topics/TopicController.java
+++ src/main/java/io/net/kukinet/springbootstarter/topics/TopicController.java
@@ -3,23 +3,20 @@ package io.net.kukinet.springbootstarter.topics;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TopicController {
 
+	@Autowired
+	// marks as need dependency injection
+	private TopicService topicService;
+	
 	@RequestMapping("/topics")
 	public List getTopics() {
-		
-		List<Topic> topics = new ArrayList<>();
-		topics.add(new Topic(1, "Spring", "Spring FW description"));
-		topics.add(new Topic(2, "Java", "Java description"));
-		topics.add(new Topic(3, "Ruby", "maybe better"));
-		
-
-			
-		return topics;	
+		return topicService.getTopics();
 	}
 	
 }
---
+++ src/main/java/io/net/kukinet/springbootstarter/topics/TopicService.java
@@ -0,0 +1,28 @@
+package io.net.kukinet.springbootstarter.topics;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+@Service
+// business service is a singleton
+public class TopicService {
+
+	
+	private List<Topic> topics;
+
+	public TopicService() {
+		topics = new ArrayList<>();
+		topics.add(new Topic(1, "Spring", "Spring FW description"));
+		topics.add(new Topic(2, "Java", "Java description"));
+		topics.add(new Topic(3, "Ruby", "maybe better"));
+	}
+	
+	
+	public List getTopics() {
+		return topics;
+	}
+
+
+}
